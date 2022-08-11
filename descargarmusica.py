8c59c656c8923a9fdb8e10142e15b39655f24154
Raul Eduardo Garnica <raulcp80@gmail.com>
2022-08-10 21:55:59 -0500

    Se agrego un menu con while

--- descargarmusica.py
+++ descargarmusica.py
@@ -1,7 +1,15 @@
 import pytube 
-link = input ("ingrese ele link del video:")
-youlink = pytube.YouTube(link)
-print("titulo:",youlink.title)
-print("Autor:",youlink.author)
-youlink.streams.filter(abr="160kbps",progressive=False).first().download(filename="audio.mp3")
-print("Descarga exitosa de :",link)
+
+opcion = int (input("selecione una opcion \n1-Descargar musica \n2-salir \n"))
+
+while opcion !=2:
+
+    link = input ("ingrese ele link del video:")
+    youlink = pytube.YouTube(link)
+    print("titulo:",youlink.title)
+    print("Autor:",youlink.author)
+    youlink.streams.filter(abr="160kbps",progressive=False).first().download(filename="audio.mp3")
+    print("Descarga exitosa de :",link)
+
+    
+    opcion = int (input("selecione una opcion \n1-Descargar musica \n2-salir \n"))
